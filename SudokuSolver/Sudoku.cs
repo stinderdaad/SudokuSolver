aa0eafd5cb99dcb07c1e704505bb1a7bebc72dc0
stinderdaad <stin@hotmail.nl>
2023-12-01 15:03:48 +0100

    sudoku squares and testdata

--- SudokuSolver/Sudoku.cs
+++ SudokuSolver/Sudoku.cs
@@ -2,19 +2,19 @@ namespace SudokuSolver;
 
 public class Sudoku
 {
-    // list of columns
+    // list of rows
     private int[,] _grid = new int[9, 9];
 
     public Sudoku(int[,] input)
     {
-        // Copy input values to the grid
-        for (int i = 0; i < 9; i++)
-        {
-            for (int j = 0; j < 9; j++)
-            {
-                _grid[i, j] = input[i, j];
-            }
-        }
+        _grid = input;
+        // for (int i = 0; i < 9; i++)
+        // {
+        //     for (int j = 0; j < 9; j++)
+        //     {
+        //         _grid[i, j] = input[i, j];
+        //     }
+        // }
     }
 
     // public int[,] GetGrid()
@@ -27,21 +27,48 @@ public class Sudoku
         int[] row = new int[9];
         for (int i = 0; i < 9; i++)
         {
-            row[i] = _grid[i, index];
+            row[i] = _grid[index, i];
         }
         return row;
     }
 
     public int[] GetColumn(int index)
     {
-        return _grid[index];
+        //return _grid[index]; why wouldn't this work?
+        int[] column = new int[9];
+        for (int i = 0; i < 9; i++)
+        {
+            column[i] = _grid[i, index];
+        }
+        return column;
+    }
+
+    // squares have index:
+    // 0 1 2
+    // 3 4 5
+    // 6 7 8
+    public int[] GetSquare(int index)
+    {
+        int[] square = new int[9];
+        int x = (index % 3) * 3;
+        int y = (index / 3) * 3;
+        int i = 0;
+        for (int j = x; j < x + 3; j++)
+        {
+            for (int k = y; k < y + 3; k++)
+            {
+                square[i] = _grid[j, k];
+                i++;
+            }
+        }
+        return square;
     }
 
     private bool RowValid(int[] row)
     {
         for (int i = 0; i < 9; i++)
         {
-            if !(row[i] > 0 && row[i] < 10){
+            if (!(row[i] > 0 && row[i] < 10)){
                 return false;
             }
 
--- SudokuSolver/SudokuSolver.cs
+++ SudokuSolver/SudokuSolver.cs
@@ -5,6 +5,24 @@ public class SudokuSolver
 {
     public static void Main(string[] args)
     {
-
+        int[,] input = new int[9, 9]
+        {
+            { 1, 2, 3, 4, 5, 6, 7, 8, 9 },
+            { 9, 8, 7, 6, 5, 4, 3, 2, 1 },
+            { 4, 5, 6, 7, 8, 9, 1, 2, 3 },
+            { 3, 1, 2, 8, 4, 5, 9, 7, 6 },
+            { 6, 9, 8, 1, 2, 3, 4, 5, 7 },
+            { 7, 4, 5, 9, 6, 1, 8, 3, 2 },
+            { 8, 6, 4, 2, 9, 7, 5, 1, 3 },
+            { 5, 7, 1, 3, 4, 8, 2, 6, 9 },
+            { 2, 3, 9, 5, 1, 6, 8, 4, 7 }
+        };
+        Sudoku sudoku = new Sudoku(input);
+        int[] row = sudoku.GetRow(0);
+        int[] column = sudoku.GetColumn(0);
+        int[] square = sudoku.GetSquare(0);
+        Console.WriteLine("Row 0: " + string.Join(", ", row));
+        Console.WriteLine("Column 0: " + string.Join(", ", column));
+        Console.WriteLine("Square 0: " + string.Join(", ", square));
     }
 }
