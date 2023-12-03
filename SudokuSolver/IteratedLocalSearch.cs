3bd1fa70807697c2589d611efc405356c6a96bf3
Knoop,K. (Kevin) <k.knoop@students.uu.nl>
2023-12-03 12:51:29 +0100

    Evaluation Function

---
+++ SudokuSolver/IteratedLocalSearch.cs
@@ -0,0 +1,19 @@
+﻿namespace SudokuSolver;
+
+public class IteratedLocalSearch
+{
+    // Check if each row and column contains the distinct numbers 1-9
+    // return the amount of missing numbers
+    private int EvaluationFunction(Sudoku sudoku)
+    {
+        var score = 0;
+        for (var i = 0; i < 9; i++)
+        {
+            var row = sudoku.GetRow(i);
+            var column = sudoku.GetColumn(i);
+            score += row.Distinct().Count();
+            score += column.Distinct().Count();
+        }
+        return score;
+    }
+}
