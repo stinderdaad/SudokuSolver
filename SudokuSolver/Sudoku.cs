6b3c48ac10687e533b39653684ce3070034cdc28
stinderdaad <stin@hotmail.nl>
2023-12-07 13:38:03 +0100

    fixed value boolean

--- SudokuSolver/Sudoku.cs
+++ SudokuSolver/Sudoku.cs
@@ -2,28 +2,35 @@ namespace SudokuSolver;
 
 public class Sudoku
 {
-    // list of rows
-    private int[,] _grid = new int[9, 9];
+    // list of rows. Tuple contains (value, fixed).
+    private (int, bool)[,] _grid = new (int, bool)[9, 9];
     private (int[] rows, int[] columns) evaluation = (new int[9], new int[9]);
 
     public int EvaluationResult { get => evaluation.rows.Sum() + evaluation.columns.Sum(); }
 
     public Sudoku(int[,] input)
     {
-        _grid = input;
-        // for (int i = 0; i < 9; i++)
-        // {
-        //     for (int j = 0; j < 9; j++)
-        //     {
-        //         _grid[i, j] = input[i, j];
-        //     }
-        // }
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                int value = input[i, j];
+                if (value == 0)
+                {
+                    _grid[i, j] = (value, false);
+                }
+                else
+                {
+                    _grid[i, j] = (value, true);
+                }
+            }
+        }
     }
 
     // constructor om een nieuw Sudoku object te maken van een bestaande Sudoku
     public Sudoku(Sudoku sudoku)
     {
-        this._grid = (int[,])sudoku._grid.Clone();
+        this._grid = ((int, bool)[,])sudoku._grid.Clone();
     }
 
     // public int[,] GetGrid()
@@ -31,9 +38,9 @@ public class Sudoku
     //     return _grid;
     // }
 
-    public int[] GetRow(int index)
+    public (int, bool)[] GetRow(int index)
     {
-        int[] row = new int[9];
+        (int, bool)[] row = new (int, bool)[9];
         for (int i = 0; i < 9; i++)
         {
             row[i] = _grid[index, i];
@@ -41,10 +48,9 @@ public class Sudoku
         return row;
     }
 
-    public int[] GetColumn(int index)
+    public (int, bool)[] GetColumn(int index)
     {
-        //return _grid[index]; why wouldn't this work?
-        int[] column = new int[9];
+        (int, bool)[] column = new (int, bool)[9];
         for (int i = 0; i < 9; i++)
         {
             column[i] = _grid[i, index];
@@ -52,13 +58,23 @@ public class Sudoku
         return column;
     }
 
+    public int[] ValuesFrom((int, bool)[] array)
+    {
+        int[] values = new int[9];
+        for (int i = 0; i < 9; i++)
+        {
+            values[i] = array[i].Item1;
+        }
+        return values;
+    }
+
     // squares have index:
     // 0 1 2
     // 3 4 5
     // 6 7 8
-    public int[] GetSquare(int index)
+    public (int, bool)[] GetSquare(int index)
     {
-        int[] square = new int[9];
+        (int, bool)[] square = new (int, bool)[9];
         int x = (index / 3) * 3;
         int y = (index % 3) * 3;
         int i = 0;
@@ -73,7 +89,7 @@ public class Sudoku
         return square;
     }
 
-    public void PutSquare(int[] square, int index)
+    public void PutSquare((int, bool)[] square, int index)
     {
         int x = (index / 3) * 3;
         int y = (index % 3) * 3;
@@ -88,64 +104,64 @@ public class Sudoku
         }
     }
 
-    private bool ArrayValid(int[] array)
-    {
-        for (int i = 0; i < 9; i++)
-        {
-            if (!(array[i] > 0 && array[i] < 10)){
-                return false;
-            }
+    // private bool ArrayValid(int[] array)
+    // {
+    //     for (int i = 0; i < 9; i++)
+    //     {
+    //         if (!(array[i] > 0 && array[i] < 10)){
+    //             return false;
+    //         }
 
-            // Check for duplicates
-            for (int j = i + 1; j < 9; j++)
-            {
-                if (array[i] == array[j])
-                {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
+    //         // Check for duplicates
+    //         for (int j = i + 1; j < 9; j++)
+    //         {
+    //             if (array[i] == array[j])
+    //             {
+    //                 return false;
+    //             }
+    //         }
+    //     }
+    //     return true;
+    // }
 
-    public bool Valid()
-    {
-        // Check rows
-        for (int i = 0; i < 9; i++)
-        {
-            if (!ArrayValid(GetRow(i)))
-            {
-                return false;
-            }
-        }
+    // public bool Valid()
+    // {
+    //     // Check rows
+    //     for (int i = 0; i < 9; i++)
+    //     {
+    //         if (!ArrayValid(GetRow(i)))
+    //         {
+    //             return false;
+    //         }
+    //     }
 
-        // Check columns
-        for (int i = 0; i < 9; i++)
-        {
-            if (!ArrayValid(GetColumn(i)))
-            {
-                return false;
-            }
-        }
+    //     // Check columns
+    //     for (int i = 0; i < 9; i++)
+    //     {
+    //         if (!ArrayValid(GetColumn(i)))
+    //         {
+    //             return false;
+    //         }
+    //     }
 
-        // Check squares
-        // Squares zijn per definitie goed, dus hoeft niet gecheckt
-        for (int i = 0; i < 9; i++)
-        {
-            if (!ArrayValid(GetSquare(i)))
-            {
-                return false;
-            }
-        }
+    //     // Check squares
+    //     // Squares zijn per definitie goed, dus hoeft niet gecheckt
+    //     for (int i = 0; i < 9; i++)
+    //     {
+    //         if (!ArrayValid(GetSquare(i)))
+    //         {
+    //             return false;
+    //         }
+    //     }
 
-        return true;
-    }
+    //     return true;
+    // }
 
     private void EvaluateRow(int row)
     {
         for (int i = 1; i < 10; i++)
         {
-            if (!GetRow(row).Contains(i))
+            if (!ValuesFrom(GetRow(row)).Contains(i))
             {
                 evaluation.rows[row]++;
             }
@@ -156,7 +172,7 @@ public class Sudoku
     {
         for (int i = 1; i < 10; i++)
         {
-            if (!GetColumn(col).Contains(i))
+            if (!ValuesFrom(GetColumn(col)).Contains(i))
             {
                 evaluation.columns[col]++;
             }
