29f34af45704889cc0fc70f44816cf09170aa14a
Knoop,K. (Kevin) <k.knoop@students.uu.nl>
2024-01-26 12:29:06 +0100

    Comments & Code-refactoring for ChronologicalBackTracking.cs

--- SudokuSolver/ChronologicalBackTracking.cs
+++ SudokuSolver/ChronologicalBackTracking.cs
@@ -3,7 +3,7 @@
 public static class ChronologicalBackTracking
 {
     // Default Sudoku to return if no solution can be found
-    public static readonly Sudoku ErrorSudoku = new Sudoku(new int[9,9], true);
+    public static readonly Sudoku ErrorSudoku = new(new int[9,9], true);
 
     // Solve a Sudoku using the Chronological Backtracking algorithm
     public static (Sudoku, int) CBT(Sudoku sudoku, Dictionary<(int, int), int[]> ranges, int iterationCount, bool fc = false, bool mcv = false)
@@ -13,7 +13,9 @@ public static class ChronologicalBackTracking
         {
             for (var j = 0; j < 9; j++)
             {
+                // Perform one step of the algorithm
                 var res = CBTStep(sudoku, ranges, ref iterationCount, ref i, ref j);
+                // If no solution can be found, return a default sudoku
                 if (res == (true, -1))
                     return (ErrorSudoku, -1);
             }
@@ -22,9 +24,8 @@ public static class ChronologicalBackTracking
         return (sudoku, iterationCount);
     }
 
-    // CBT for one cell of the sudoku, returns true if terminated and 0 with or -1 without solution, and false otherwise
-    // iterationCount is passed by reference so that calling method can keep track of it
-    // row and col are passed by ref so that it is consistent with the for loops in the calling methods of CBT and FC 
+    // CBT for one cell of the sudoku, returns (true,0) if succesful, (true,-1) if no solution can be found, (false,0) if the cell is fixed
+    // row and col are passed by reference so that it is consistent with the for loops in the calling methods of CBT and FC 
     public static (bool, int) CBTStep(Sudoku sudoku, Dictionary<(int, int), int[]> ranges, ref int iterationCount,
         ref int row, ref int col, bool fc = false, bool mcv = false, (int, int)[]? array = null)
     {
@@ -40,18 +41,19 @@ public static class ChronologicalBackTracking
         sudoku.Grid[row, col] = new SudokuItem(range[counter], false);
         iterationCount++;
 
+        // If we are using forward checking, we need to update the ranges
         if (fc)
             ForwardChecking.UpdateRangesFC(sudoku, row, col, ranges);
             
         // Check if the current sudoku layout is valid
-            while (!Check(sudoku, row, col) || ForwardChecking.IsEmptyRangeSomewhere(ranges)) 
+        while (!Check(sudoku, row, col) || ForwardChecking.IsEmptyRangeSomewhere(ranges)) 
         {
             // If it is not valid, we put the next possible value in the cell
             counter++;
             
             // If we have tried all possible values, we need to backtrack
             range = ranges[(row, col)];
-            if (counter >= range.Length) // i.e. if counter is the last number in the range
+            if (counter >= range.Length)
             {
                 // Backtracking looks different in each algorithm
                 if (fc)
@@ -59,7 +61,7 @@ public static class ChronologicalBackTracking
                 
                 else (row, col) = CBackTrack(sudoku, row, col); 
                 
-                // if we go all the way back to the beginning, return -1 to signal no solution found
+                // If we go all the way back to the beginning, return -1 to signal no solution found
                 if (row == -1) return (true, -1);
                 
                 // Set the counter to the subsequent value of the current cell
@@ -82,9 +84,10 @@ public static class ChronologicalBackTracking
             iterationCount++;
         }
 
-        return (true, 0); // succeeded, ready for next cell
+        return (true, 0); // Succeeded, ready for next cell
     }
 
+    // Backtrack function for the CBT algorithm
     private static (int, int) CBackTrack(Sudoku sudoku, int row, int col)
     {
         // Set the current cell back to 0
@@ -128,12 +131,13 @@ public static class ChronologicalBackTracking
         return (row, col);
     }
 
+    // Backtrack function for the FC algorithm
     private static (int, int) FCBackTrack(Sudoku sudoku, Dictionary<(int, int), int[]> ranges, int row, int col)
     {
         // Set the current cell back to 0
         sudoku.Grid[row, col] = new SudokuItem(0, false);
         
-        // ReAdd the now unused value to the ranges
+        // Re-add the now unused value to the ranges
         ForwardChecking.MakeConsistent(sudoku, ranges);
 
         // If we are in the first column, we need to go to the previous row or terminate
@@ -184,7 +188,7 @@ public static class ChronologicalBackTracking
         return (row, col);
     }
     
-    // Function to check that the current sudoku layout is valid, given a row, column, and square index
+    // Function to check that the current sudoku layout is valid, given a row and column
     private static bool Check(Sudoku sudoku, int row, int column)
     {
         // Determine the square index of the current cell
@@ -206,8 +210,6 @@ public static class ChronologicalBackTracking
         return squareValues.Where(item => item.Number != 0).All(item => squareSet.Add(item.Number));
     }
     
-    
-
     // Generate ranges for empty cells for use in CBT
     public static Dictionary<(int, int), int[]> GenerateRangesCBT(Sudoku sudoku, Dictionary<(int, int), int[]> ranges)
     {
@@ -217,7 +219,7 @@ public static class ChronologicalBackTracking
             {
                 if (sudoku.Grid[i, j].IsFixed || sudoku.Grid[i,j].Number != 0)
                     continue;
-                
+                // CBT uses default ranges of 1-9 for empty cells
                 ranges[(i, j)] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
             }
         }
