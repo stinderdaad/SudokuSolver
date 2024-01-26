ec47ed951378c6b896640a8e2cfd56e23bf241ea
Knoop,K. (Kevin) <k.knoop@students.uu.nl>
2024-01-26 12:37:49 +0100

    Comments & Code-refactoring for Program.cs

--- SudokuSolver/Program.cs
+++ SudokuSolver/Program.cs
@@ -12,46 +12,44 @@ public class Program
         // Input can be provided in 2 ways:
         // 1. As Console input (like is the case below)
         // 2. As a string representation of a Sudoku puzzle (row for row)
-        // If you wish to use case 2, comment out line 41 and 42 and uncomment line 43 and (re)define a sudokuString variable
-        //
-        // BuildSudoku takes 2 arguments: the input array and a boolean that indicates whether
-        //  the input is fixed or not.
-        // If the boolean is set to False, then no numbers of the input will be set to fixed.
-        //  This way the program could solve fully filled in sudoku's for example.
-        // If the boolean is set to True, then every non-zero number of the input will be set to fixed
-        //
-        // The application then prints the input sudoku in the Console followed by its initial evaluation value
+        // If you wish to use case 2:
+        //  Uncomment the sudokuString variable and switch the comments for the inputArray variables
+
+        // The application prints the input sudoku in the Console
         //  (red numbers are fixed, values at the end of rows/columns are the respective evaluation values for those rows/columns)
-        // After that we solve the sudoku using the Iterated Local Search algorithm.
-        // Solve takes 3 arguments: the sudoku to solve, the sValue and the maximum number of iterations.
-        // The sValue is the number of times you want to execute random-walk.
-        // The maximum number of iterations is the maximum number of iterations the algorithm will
-        //  run before automatically terminating.
+        //  The evaluation values are not applicable to P2 and are only used in P1, but they are still present in the visualisation
+        // After that we can solve the sudoku using one of the 3 algorithms that are implemented.
+        //  In order to run one of the algorithms, uncomment the corresponding line (and comment the others)
+        //  All algorithms take the following 3 parameters: the sudoku, the ranges, and the iteration count
+        //  Only the ranges for CBT should be generated, the other algorithms will generate their own ranges from an empty Dictionary
         
-        // After this is done, the solution will be printed to the Console.
+        // After this is done, the solution will be printed to the Console as well as the amount of value assignments
+        // (iterations) it took.
         //**********************************//
         //**********************************//
-        
-        var solver = new SudokuSolver();
         
         /*const string sudokuString = "0 0 3 0 2 0 6 0 0 9 0 0 3 0 5 0 0 1 0 0 1 8 0 6 4 0 0 0 0 8 1 0 2 9" +
                                     " 0 0 7 0 0 0 0 0 0 0 8 0 0 6 7 0 8 2 0 0 0 0 2 6 0 9 5 0 0 8 0 0 2 0 3 0 " +
                                     "0 9 0 0 5 0 1 0 3 0 0";*/
         
         var inputArray = SudokuSolver.GetInput();
+        //var inputArray = sudokuString.Split(' ');
         var numberArray = SudokuSolver.PopulateArray(inputArray);
         var sudoku = new Sudoku(numberArray, true);
         Console.WriteLine("Sudoku:");
         sudoku.Print();
         var ranges = new Dictionary<(int, int), int[]>();
-        // var solution = ChronologicalBackTracking.CBT(sudoku, ranges, 0);
-        var solution = ForwardChecking.MCV(sudoku, ranges, 0);
+        var solution = 
+            ChronologicalBackTracking.CBT(sudoku, ChronologicalBackTracking.GenerateRangesCBT(sudoku,ranges), 0);
+        //var solution = ForwardChecking.FC(sudoku, ranges, 0);
+        //var solution = ForwardChecking.MCV(sudoku, ranges, 0);
 
         Console.WriteLine("Solution:");
         solution.Item1.Print();
         Console.WriteLine($"Iterations: {solution.Item2}");
 
         //************************P1************************//
+        // var solver = new SudokuSolver();
         // var sudoku = solver.BuildSudoku(inputArray, true);
         // //var sudoku = solver.BuildSudoku(sudokuString.Split(' '), true);
         //
